76572c958657eb5ecdc0dbd5a1ae26c6e66f1258
JaseGrable <jase.grable@gmail.com>
2023-08-13 21:01:03 -0700

    Fix erroprs in Flavors Controller

--- SweetAndSavory/Controllers/FlavorsController.cs
+++ SweetAndSavory/Controllers/FlavorsController.cs
@@ -7,7 +7,7 @@ using System.Linq;
 
 namespace SweetAndSavory.Controllers
 {
-    public class FlavorController : Controller
+    public class FlavorsController : Controller
     {
         private readonly SweetAndSavoryContext _db;
 
@@ -15,48 +15,43 @@ namespace SweetAndSavory.Controllers
         {
             _db = db;
         }
+
         public ActionResult Index()
         {
-            List<FlavorController> model = _db.Flavors.ToList();
+            List<Flavor> model = _db.Flavors.ToList();
             ViewBag.PageTitle = "View Flavors";
             return View(model);
         }
 
         public ActionResult Create()
         {
-            ViewBag.PageTitle = "Add Flavor";
             return View();
         }
-    }
-    [HttpPost]
-    public ActionResult Create(Flavor flavor)
-    {
-        if (!ModelState.IsValid)
-        {
-            return View(flavor);
-        }
-        else
+
+        [HttpPost]
+        public ActionResult Create(Flavor flavor)
         {
             _db.Flavors.Add(flavor);
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
+
         public ActionResult Details(int id)
         {
             Flavor thisFlavor = _db.Flavors
-                            .Include(flavor => flavor.JoinEntities)
-                            .ThenInclude(join => join.Treat)
-                            .FirstOrDefault(flavor => flavor.FlavorId == id);
+                              .Include(flavor => flavor.JoinEntities)
+                              .ThenInclude(join => join.Treat)
+                              .FirstOrDefault(flavor => flavor.FlavorId == id);
             ViewBag.PageTitle = "Flavor Details";
             return View(thisFlavor);
         }
         public ActionResult Edit(int id)
         {
-            Flavor thisFlavor = _db.Flavors
-                            .FirstOrDefault(flavor => flavor.FlavorId == id);
+            Flavor thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
             ViewBag.PageTitle = "Edit Flavor";
             return View(thisFlavor);
         }
+
         [HttpPost]
         public ActionResult Edit(Flavor flavor)
         {
@@ -66,36 +61,33 @@ namespace SweetAndSavory.Controllers
         }
         public ActionResult Delete(int id)
         {
-            Flavor thisFlavor = _db.Flavors
-                            .FirstOrDefault(flavor => flavor.FlavorId == id);
+            Flavor thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
             ViewBag.PageTitle = "Delete Flavor";
             return View(thisFlavor);
         }
+
         [HttpPost, ActionName("Delete")]
         public ActionResult DeleteConfirmed(int id)
         {
-            Flavor thisFlavor = _db.Flavors
-                            .FirstOrDefault(flavor => flavor.FlavorId == id);
+            Flavor thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
             _db.Flavors.Remove(thisFlavor);
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
         public ActionResult AddTreat(int id)
         {
-            Flavor thisFlavor = _db.Flavors
-                            .FirstOrDefault(flavors => flavors.FlavorId == id);
+            Flavor thisFlavor = _db.Flavors.FirstOrDefault(flavors => flavors.FlavorId == id);
             List<Treat> treats = _db.Treats.ToList();
             SelectList treatList = new SelectList(treats, "TreatId", "TreatName");
-            ViewBag.PageTitle = "Add Flavor to Treat";
             ViewBag.TreatId = treatList;
             return View(thisFlavor);
         }
+
         [HttpPost]
         public ActionResult AddTreat(Flavor flavor, int treatId)
         {
 #nullable enable
-            FlavorTreat? joinEntity = _db.FlavorTreats
-                                .FirstOrDefault(join => (join.TreatId == treatId && join.FlavorId == flavor.FlavorId));
+            FlavorTreat? joinEntity = _db.FlavorTreats.FirstOrDefault(join => (join.TreatId == treatId && join.FlavorId == flavor.FlavorId));
 #nullable disable
             if (joinEntity == null && treatId != 0)
             {
@@ -106,4 +98,3 @@ namespace SweetAndSavory.Controllers
         }
     }
 }
-
